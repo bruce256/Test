1cef00a521461a9ac58192a64370f8d2f7012dae
lvsheng <lvsheng.ls@alibaba-inc.com>
2018-04-24 00:23:52 +0800

    xmlbeans

---
+++ src/main/java/xmlbeans/PurchaseOrderDocumentTest.java
@@ -0,0 +1,28 @@
+package xmlbeans;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.xmlbeans.XmlException;
+import org.openuri.easypo.PurchaseOrderDocument;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * @auther 儒尊
+ * @date 2018/4/23
+ **/
+public class PurchaseOrderDocumentTest {
+	
+	public static void main(String[] args) {
+		try {
+			PurchaseOrderDocument               purchaseOrderDocument = PurchaseOrderDocument.Factory.parse(new File("/work/github/Test/src/main/resources/easy.xml"));
+			PurchaseOrderDocument.PurchaseOrder purchaseOrder         = purchaseOrderDocument.getPurchaseOrder();
+			System.out.println(purchaseOrder.getCustomer().getName());
+			System.out.println(purchaseOrder.toString());
+		} catch (XmlException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
