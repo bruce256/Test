0a50f207e5de4578dbfaebabc46f3b659c2d98c1
lvsheng1 <326375225@qq.com>
2023-10-12 10:22:45 +0800

    Merge branch 'master' of https://github.com/bruce256/Test
    # Conflicts:
    #	src/main/java/HelloWorld.kt

--- src/main/java/cglib/DoCGLib.java
+++ src/main/java/cglib/DoCGLib.java
@@ -1,5 +1,7 @@
 package cglib;
 
+import net.sf.cglib.core.DebuggingClassWriter;
+
 /**
  * @author 儒尊
  * @date 2017/05/07
@@ -7,6 +9,7 @@ package cglib;
 public class DoCGLib {
 
 	public static void main(String[] args) {
+		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY, "/Users/lvsheng1/Downloads");
 		CglibProxy proxy = new CglibProxy();
 		//通过生成子类的方式创建代理类
 		SayHello proxyImp = (SayHello) proxy.getProxy(SayHello.class);
