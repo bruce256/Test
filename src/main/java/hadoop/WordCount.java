5357b95e389b4183c9dbe3a62e91ec0e1d890614
“bruce256” <326375225@qq.com>
2024-04-29 22:45:24 +0800

    注释

--- src/main/java/hadoop/WordCount.java
+++ src/main/java/hadoop/WordCount.java
@@ -62,21 +62,29 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         long          start = System.currentTimeMillis();
 
+        // 1. 获取配置文件对象，获取job对象实例
         Configuration conf  = new Configuration();
         Job           job   = Job.getInstance(conf, "word count");
+
+        // 2. 指定程序jar的本地路径
         job.setJarByClass(WordCount.class);
 
+        //  3. 指定Mapper/Reducer类
         job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
         job.setReducerClass(IntSumReducer.class);
 
+        // 5. 指定最终输出的kv数据类型
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
 
+        // 6. 指定job处理的原始数据路径
         FileInputFormat.addInputPath(job, new Path("/Users/LvSheng/code/github/Test/data.txt"));
+        //  7. 指定job输出结果路径
         FileOutputFormat.setOutputPath(job, new Path("/Users/LvSheng/code/github/Test/data_out"));
 
+        //  8. 提交作业
         job.waitForCompletion(true);
+
         System.out.println("time cost ： " + (System.currentTimeMillis() - start) / 1000 + " s");
     }
 }
