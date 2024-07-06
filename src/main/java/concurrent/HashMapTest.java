b793e8810b966f73315a4cc69d1ef7e87fd53629
“bruce256” <326375225@qq.com>
2024-07-06 22:50:41 +0800

    hashmap并发问题

--- src/main/java/concurrent/HashMapTest.java
+++ src/main/java/concurrent/HashMapTest.java
@@ -7,12 +7,16 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * hashmap在并发情况下的问题
+ * 1，扩容时，并发读数据可能读不到
+ * 2. 并发写数据可能会丢失
  *
  * @auther: LvSheng
  * @date: 2024/6/18
  * @description:
  */
 public class HashMapTest {
+
+    public static volatile int missingCount = 0;
 
     public static void main(String[] args) {
         HashMap<Integer, Integer> map      = new HashMap<>();
@@ -21,21 +25,29 @@ public class HashMapTest {
             new LinkedBlockingQueue<Runnable>(100),
             new ThreadPoolExecutor.CallerRunsPolicy());
 
-        int max = 10000000;
-        for (int i = 0; i < max; i++) {
+        int max            = 10000000;
+        int testMissingKey = 10001;
+        map.put(testMissingKey, 0);
+        for (int i = 1; i < max; i++) {
             int finalI = i;
             es.submit(() -> {
                 System.out.println(finalI);
                 map.put(finalI, finalI);
+                Integer value = map.get(testMissingKey);
+                if (value == null) {
+                    missingCount++;
+                }
             });
         }
 
         try {
             es.shutdown();
+            es.awaitTermination(1, TimeUnit.MINUTES);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         System.out.println((max - map.size()) + "个数字缺失了");
+        System.out.println(missingCount + "次并发读失败");
     }
 }
